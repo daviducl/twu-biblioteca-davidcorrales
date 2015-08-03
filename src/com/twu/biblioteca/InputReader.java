13490e8b1cf43797dc0d67dfeaa9085c98f32648
daviducl <mcorrale@thoughtworks.com>
2015-08-02 21:33:00 -0500

    5th user story. Numerical input validation implemented. All tests, except Movie related ones pass

--- src/com/twu/biblioteca/InputReader.java
+++ src/com/twu/biblioteca/InputReader.java
@@ -10,10 +10,12 @@ import java.io.InputStreamReader;
 public class InputReader {
     public static int readInteger(String prompt) {
         int inputLine = -1;
+        String regEx = "[0-9]*";
         System.out.print(prompt);
         try {
             BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
-            inputLine = Integer.parseInt(is.readLine());
+            if (is.readLine().matches(regEx))
+                inputLine = Integer.parseInt(is.readLine());
         } catch (IOException e) {
             System.err.println("Select a valid option!");
         }
